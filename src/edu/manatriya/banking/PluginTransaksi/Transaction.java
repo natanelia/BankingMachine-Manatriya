12f5581435dd7013dd1d656b89e5bf1be7f4932c
Natan <natanelia7@gmail.com>
2015-04-12 19:24:34 +0700

    First Commit

---
+++ src/edu/manatriya/banking/PluginTransaksi/Transaction.java
@@ -0,0 +1,16 @@
+package edu.manatriya.banking.PluginTransaksi;
+
+/**
+ * Created by Natan Elia on 4/12/2015.
+ */
+public class Transaction {
+    private Account acc;
+
+    public void execute() {
+        
+    }
+
+    public void stop() {
+
+    }
+}
