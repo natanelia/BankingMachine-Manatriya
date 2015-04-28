ddea6dc2446421c287f5343165b18cfe8b580e1b
KevinYauris <kyauris@gmail.com>
2015-04-29 00:03:51 +0700

    Account JUnit
    getpassword sama getname ngaco... tp nggak tahu it emank karena di
    laptopku atau emank bener, coba ditest di kalian aj y...

---
+++ src/edu/manatriya/banking/ATMMachineTest.java
@@ -0,0 +1,16 @@
+package edu.manatriya.banking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by KEVIN on 28/04/2015.
+ */
+public class ATMMachineTest {
+
+    @Test
+    public void testRun() throws Exception {
+        ATMMachine dummy = new ATMMachine();
+    }
+}
---
+++ src/edu/manatriya/banking/akunbanking/AccountFactoryTest.java
@@ -0,0 +1,19 @@
+package edu.manatriya.banking.akunbanking;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by KEVIN on 28/04/2015.
+ */
+public class AccountFactoryTest {
+
+    @Test
+    public void testGetAccount() throws Exception {
+        AccountFactory accountfactorytest = new AccountFactory();
+        Account dummy;
+        dummy = accountfactorytest.getAccount("out\\Accounts\\DE901366.acc");
+        assertEquals("DE901366", dummy.getAccountID());
+    }
+}
