a0380e8218c914faf7d615764f639e8787db3c83
KevinYauris <kyauris@gmail.com>
2015-04-29 15:51:58 +0700

    +account dummy
    account dummyy

---
+++ src/edu/manatriya/banking/akunbanking/AccountAutoSaverTest.java
@@ -0,0 +1,34 @@
+package edu.manatriya.banking.akunbanking;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by KEVIN on 28/04/2015.
+ */
+public class AccountAutoSaverTest {
+
+    @Test
+    public void testRun() throws Exception {
+        Account account = new DebitAccount("out/Accounts/DEdummy.acc");
+        AccountAutoSaver accAutoSaver = new AccountAutoSaver(account, 5);
+        accAutoSaver.start();
+        account.setSaldo(10000);
+        File fileTransaction = new File("out/Accounts/DEdummy.acc");
+        Scanner accountScanner = new Scanner(fileTransaction);
+        String name, password, currency;
+        double saldo;
+        password = accountScanner.nextLine();
+        name = accountScanner.nextLine();
+        currency = accountScanner.nextLine();
+        saldo = Double.parseDouble(accountScanner.nextLine());
+        assertEquals("012345", password);
+        assertEquals("EUR", currency);
+        assertEquals(10000.0, saldo, 0);
+
+    }
+}
