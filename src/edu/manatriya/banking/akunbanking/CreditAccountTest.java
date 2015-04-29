38f5c0a37d2f61d57346606b37822ddbdeaae3de
KevinYauris <kyauris@gmail.com>
2015-04-29 15:13:55 +0700

    Revert "accountdummy untuk testing"
    This reverts commit 9f62be6ae43e48aad4be311567223910a4d35142.

--- src/edu/manatriya/banking/akunbanking/CreditAccountTest.java
+++ src/edu/manatriya/banking/akunbanking/CreditAccountTest.java
@@ -11,7 +11,7 @@ public class CreditAccountTest{
 
     @Test
     public void testUpdateSaldo() throws Exception {
-        CreditAccount dummy = new CreditAccount("out/Accounts/CRdummy.acc");
+        CreditAccount dummy = new CreditAccount("out/Accounts/CR790112.acc");
         double tempsaldo=dummy.getSaldo();
         dummy.updateSaldo(-10000);
         assertEquals(tempsaldo-10000,dummy.getSaldo(),0);
