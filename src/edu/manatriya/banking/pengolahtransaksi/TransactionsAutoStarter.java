35f0dc38367bae6e3561026a022287aa9a095564
alberttriadrian <alberttri_9e_17@yahoo.com>
2015-05-06 17:07:20 +0700

    Update TransactionsAutoStarter.java

--- src/edu/manatriya/banking/pengolahtransaksi/TransactionsAutoStarter.java
+++ src/edu/manatriya/banking/pengolahtransaksi/TransactionsAutoStarter.java
@@ -1,16 +1,24 @@
 package edu.manatriya.banking.pengolahtransaksi;
 
 /**
- * Created by Natan Elia on 4/23/2015.
+ * Kelas TransactionAutoStarter akan secara otomatis menjalankan semua transaksi setelah selang waktu tertentu
  */
 public class TransactionsAutoStarter extends Thread {
     private TransactionProcessor transactionProcessor;
     private int interval;
 
+    /**
+     * Constructor Transaction Auto Starter
+     * @param transactionProcessor TransactionProcesor
+     * @param intervalInSecond interval waktu untuk memulai transaksi secara otomatis
+     */
     public TransactionsAutoStarter(TransactionProcessor transactionProcessor, int intervalInSecond) {
         this.transactionProcessor = transactionProcessor;
     }
 
+    /**
+     * Menjalan semua transaksi secara otomatis setelah selang waktu tertentu
+     */
     @Override
     public synchronized void run() {
         try {
